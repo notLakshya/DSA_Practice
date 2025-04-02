6f7bd20db547ed1ec0ce65e08fe9eaacc17a74be
notLakshya <64923683+notLakshya@users.noreply.github.com>
2025-04-02 14:10:00 +0530

    DLL methods added

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -32,5 +32,91 @@ public class DoublyLinkedList {
         System.out.println("length: "+ length);
         return length;
     }
+
+    public void printList(){
+        Node temp = head;
+
+        System.out.println("Printing the DLL: ");
+
+        while(temp!=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        length++;
+    }
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+        length++;
+    }
+    public Node removeLast(){
+
+        if(length == 0) return null;
+        Node temp = tail;
+        if(length == 1){
+            head = null;
+            tail = null;
+        } else{
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+    public Node removeFirst(){
+        if(length == 0) return null;
+        Node temp = head.next;
+        if(length == 1){
+            head = null;
+            tail = null;
+        }else{
+          temp.prev = null;
+          head.next = null;
+          head = temp;
+        }
+        length--;
+
+        return temp;
+
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList myLinkedList = new DoublyLinkedList(4);
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.append(2);
+        myLinkedList.append(12);
+        myLinkedList.append(33);
+        myLinkedList.append(8);
+
+        myLinkedList.removeLast();
+
+        myLinkedList.printList();
+        myLinkedList.prepend(10);
+        myLinkedList.removeFirst();
+        myLinkedList.printList();
+
+
+    }
     
 }
