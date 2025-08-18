4b9d0d106c6d592158667502d125837cb3cabce1
notLakshya <64923683+notLakshya@users.noreply.github.com>
2025-08-18 11:36:31 +0530

    added remaining sort and bit codes

---
+++ DSA/Algorithms/Sort/CyclicSort.java
@@ -0,0 +1,24 @@
+package DSA.Algorithms.Sort;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 2};
+        cyclicSort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void cyclicSort(int[] arr){
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1; // Calculate the correct index for the current element
+            if (arr[i] != arr[correctIndex]) {
+                // Swap arr[i] and arr[correctIndex]
+                 
+            } else {
+                i++; // Move to the next element if the current element is in the correct position
+            }
+        }
+    }
+}
